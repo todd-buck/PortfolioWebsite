e09430484ebb471cdeb52c4bceebd3b22df54ccc
todd-buck <btbuckjr@gmail.com>
2023-04-27 00:32:02 -0500

    Fixed project scaffolding/introduced new icons

--- src/components/global/Sidebar.js
+++ src/components/global/Sidebar.js
@@ -1,9 +1,13 @@
 import Box from "@mui/material/Box"
+import MenuIcon from '@mui/icons-material/Menu';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 const Sidebar = () => {
     return (
         <Box sx={{backgroundColor: '#B1A296', height: '100vh', border: 2, borderColor: '#756457'}}>
-            this is a sidebar
+            <MenuIcon />
+            
         </Box>
     )
 }
